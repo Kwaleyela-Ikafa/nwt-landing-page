a4d12b9cc524d61db7428763cca70a33d4b49009
Kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2024-03-25 20:01:33 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Home from "./Components/Home";
+import Introduction from "./Components/Introduction";
+import Market from "./Components/Market";
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+      <Introduction />
+      <Market />
+    </div>
+  );
+}
+
+export default App;
