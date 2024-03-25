06dcc75f1758cf55a213bc87c9b62801c09ccd7d
Kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2024-03-25 20:01:49 +0200

    Home component

---
+++ src/Components/Home.js
@@ -0,0 +1,34 @@
+import React from "react";
+import BannerBackground from "../Assets/home-banner-background.png";
+import BannerImage from "../Assets/home-banner-image.png";
+import Navbar from "./Navbar";
+import { FiArrowDown } from "react-icons/fi";
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <Navbar />
+      <div className="home-banner-container">
+        <div className="home-bannerImage-container">
+          <img src={BannerBackground} alt="" />
+        </div>
+        <div className="home-text-section">
+          <h2 className="primary-heading">
+            Nourishing Western Kenya (NWK) Brand Creation - Marketing Communications
+          </h2>
+          <p className="primary-text">
+            Unveiling the Millet Market in France: Popularity, Potential, and Promotion
+          </p>
+          <button className="secondary-button">
+            Read More <FiArrowDown />{" "}
+          </button>
+        </div>
+        <div className="home-image-section">
+          <img src={BannerImage} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
